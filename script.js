4b641f56820c9592a4460e3cc22c8ed5db3c1684
ShakespeareChabungbam <116629389+ShakespeareChabungbam@users.noreply.github.com>
2024-04-18 02:11:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const title = document.querySelector('.title');
+    const leaf1 = document.querySelector('.leaf1');
+    const leaf2 = document.querySelector('.leaf2');
+    const bush2 = document.querySelector('.bush2');
+    const mount1 = document.querySelector('.mount1');
+    const mount2 = document.querySelector('.mount2');
+
+    document.addEventListener('scroll', function() {
+        let value = window.scrollY;
+        // console.log(value)
+        title.style.marginTop = value * 1.1 + 'px';
+        leaf1.style.marginLeft = -value + 'px';
+        leaf2.style.marginLeft = value + 'px';
+        bush2.style.marginBottom = -value + 'px';
+        mount1.style.marginBottom = -value * 1.1 + 'px';
+        mount2.style.marginBottom = -value * 1.2 + 'px';
+    });
+
+    const passwordInput = document.getElementById('password');
+    const verifyPasswordInput = document.getElementById('verify-password');
+    const passwordError = document.getElementById('password-error');
+
+    verifyPasswordInput.addEventListener('input', function() {
+        if (passwordInput.value !== verifyPasswordInput.value) {
+            passwordError.classList.remove('hidden');
+        } else {
+            passwordError.classList.add('hidden');
+        }
+    });
+});
